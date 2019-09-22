eabfe58e078ec1e1a647a1d4a800264e26d49354
金彰海 <akiumikin@gmail.com>
2019-09-22 18:24:01 +0900

    componentの読み込みでエラーになっていたので、一旦直書きに変更

--- react_app/src/routeItem.tsx
+++ react_app/src/routeItem.tsx
@@ -1,29 +1,12 @@
 // react
 import * as React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-
-// @material-ui
-import MenuItem from '@material-ui/core/MenuItem';
+import { Switch, Route } from 'react-router-dom';
 
 // components for links
 import { TopPage } from './components/TopPage/TopPage';
 import { User } from './components/User/User';
 import { WithoutAppBarPage } from './components/WithoutAppBarPage/WithoutAppBarPage';
 import { NotFound } from './components/others/NotFound';
-
-/**
- * サービスの上部のBarに含まれるリンクを設定
- * @param handleClose リンクのウィンドウを閉じる関数
- */
-export const AppBarItem = (handleClose: any) => {
-  return (
-    <>
-      <MenuItem onClick={handleClose}><Link to='/'>TopPage</Link></MenuItem>
-      <MenuItem onClick={handleClose}><Link to='/user'>User</Link></MenuItem>
-      <MenuItem onClick={handleClose}><Link to='/without_app_bar_page'>WithoutAppBarPage</Link></MenuItem>
-    </>
-  );
-}
 
 /**
  * react内のルーティングの設定
